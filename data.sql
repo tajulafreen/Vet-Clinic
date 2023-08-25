e5f2d876c4dba2cad34fa017e84eff81ead28ade
afreen shaik <affushaik9493@gmail.com>
2023-08-25 16:04:08 +0530

    update data, schema and queries.sql file

--- data.sql
+++ data.sql
@@ -34,3 +34,63 @@ VALUES (10, 'Blossom', '1998-10-13', 3, TRUE, 17);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (11, 'Ditto', '2022-05-14', 4, TRUE, 22);
+
+
+-- Insert the following data into the owners table:
+
+--     Sam Smith 34 years old.
+--     Jennifer Orwell 19 years old.
+--     Bob 45 years old.
+--     Melody Pond 77 years old.
+--     Dean Winchester 14 years old.
+--     Jodie Whittaker 38 years old.
+
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+
+-- Insert the following data into the species table:
+
+--     Pokemon
+--     Digimon
+
+INSERT INTO species (name)
+VALUES
+    ('Pokemon'),
+    ('Digimon');
+
+
+-- Modify your inserted animals so it includes the species_id value:
+
+--     If the name ends in "mon" it will be Digimon
+--     All other animals are Pokemon
+
+UPDATE animals
+SET species_id = CASE
+    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END;
+
+
+-- Modify your inserted animals to include owner information (owner_id):
+
+--     Sam Smith owns Agumon.
+--     Jennifer Orwell owns Gabumon and Pikachu.
+--     Bob owns Devimon and Plantmon.
+--     Melody Pond owns Charmander, Squirtle, and Blossom.
+--     Dean Winchester owns Angemon and Boarmon.
+
+UPDATE animals
+SET owner_id = CASE
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name IN ('Gabumon', 'Pikachu') THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name IN ('Devimon', 'Plantmon') THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;
--- schema.sql
+++ schema.sql
@@ -10,3 +10,53 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+-- Create a table named owners with the following columns:
+
+--     id: integer (set it as autoincremented PRIMARY KEY)
+--     full_name: string
+--     age: integer
+
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name TEXT,
+  age INTEGER
+);
+
+
+-- Create a table named species with the following columns:
+
+--     id: integer (set it as autoincremented PRIMARY KEY)
+--     name: string
+
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+
+-- Modify animals table:
+
+--     Make sure that id is set as autoincremented PRIMARY KEY
+--     Remove column species
+--     Add column species_id which is a foreign key referencing species table
+--     Add column owner_id which is a foreign key referencing the owners table
+
+
+ALTER TABLE animals ADD COLUMN new_id serial;
+
+UPDATE animals SET new_id = id;
+
+ALTER TABLE animals DROP COLUMN id;
+
+ALTER TABLE animals RENAME COLUMN new_id TO id;
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
